cef2d92c42b8fe57ebc99c4a0bdce6850d927680
aya <aya2103436@miuegypt.edu.eg>
2024-03-11 02:10:06 +0200

    validations login

--- demo/src/main/java/com/example/aswe/demo/Controllers/UserController.java
+++ demo/src/main/java/com/example/aswe/demo/Controllers/UserController.java
@@ -16,75 +16,23 @@ import org.springframework.web.servlet.ModelAndView;
 import com.example.aswe.demo.Models.User;
 import com.example.aswe.demo.Repositories.UserRepository;
 
-// @RestController
-
-
-// public class UserController {
-//      @Autowired
-//     private UserRepository userRepository;
-//     @GetMapping("")
-    
-//     public ModelAndView getUsers() {
-//         ModelAndView mav = new ModelAndView("index.html");
-//         List<User>users = this.userRepository.findAll();
-//         mav.addObject("users", users);
-//         return mav;
-//     }
-
-//      @GetMapping("/signup")
-//     public ModelAndView addUser() {
-//         ModelAndView mav = new ModelAndView("signup.html");
-//         User newUser = new User();
-//         mav.addObject("user", newUser);
-//         return mav;
-//     }
-    
-//     @PostMapping("/signup")
-// public ModelAndView saveUser(@ModelAttribute User user) {
-//     String encodedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(12));
-//     user.setPassword(encodedPassword);
-//     this.userRepository.save(user);
-
-//     ModelAndView mav = new ModelAndView("redirect:/login");
-//     return mav;
-// }
-    
-//     @GetMapping("/login")
-//     public ModelAndView login() {
-//         ModelAndView mav = new ModelAndView("login.html");
-//         User newUser = new User();
-//         mav.addObject("user", newUser);
-//         return mav;
-//     }
-//      @PostMapping("/login")
-//     public String LoginProcess(@RequestParam("username") String username, @RequestParam("password") String password) {
-//        User dbUser = this.userRepository.findByUsername(username);
-//        if(dbUser == null) return "Login Failed";
-//        Boolean isPasswordMatched = BCrypt.checkpw(password, dbUser.getPassword());
-//        if(isPasswordMatched)
-//         return "Welcome " + dbUser.getFullname();
-//         else
-//         return "Login Failed";
-//     }
- 
-    
-// }
-
-
-
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
-
-    @GetMapping("")
-    public ModelAndView getUsers() {
-        ModelAndView mav = new ModelAndView("index.html");
-        List<User> users = this.userRepository.findAll();
-        mav.addObject("users", users);
-        return mav;
+    
+    @GetMapping("/index")
+    public ModelAndView index() {
+        return new ModelAndView("index.html");
     }
+    @GetMapping("/")
+public ModelAndView getUsers() {
+    ModelAndView mav = new ModelAndView("index.html");
+    List<User> users = this.userRepository.findAll();
+    mav.addObject("users", users);
+    return mav;
+}
 
     @GetMapping("/signup")
     public ModelAndView addUser() {
@@ -164,6 +112,10 @@ public class UserController {
         return password != null && password.length() >= 8 && password.equals(confirmPassword);
     }
 
+   
+    
+
+
     @GetMapping("/login")
     public ModelAndView login() {
         ModelAndView mav = new ModelAndView("login.html");
@@ -173,19 +125,31 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String loginProcess(@RequestParam("username") String username, @RequestParam("password") String password) {
-        User dbUser = this.userRepository.findByUsername(username);
-        if (dbUser == null) return "Login Failed";
-        Boolean isPasswordMatched = BCrypt.checkpw(password, dbUser.getPassword());
-        if (isPasswordMatched)
-            return "Welcome " + dbUser.getFullname();
-        else
-            return "Login Failed";
+public ModelAndView loginProcess(@RequestParam("username") String username, @RequestParam("password") String password) {
+    ModelAndView mav = new ModelAndView("login.html");
+
+    if (username == null || password == null) {
+        mav.addObject("loginError", "Please provide both username and password");
+        return mav;
     }
 
+    User dbUser = userRepository.findByUsername(username);
+    if (dbUser == null) {
+        mav.addObject("loginError", "Username not found");
+        mav.addObject("loginErrorField", "username");
+        return mav;
+    }
+
+    boolean isPasswordMatched = BCrypt.checkpw(password, dbUser.getPassword());
+    if (!isPasswordMatched) {
+        mav.addObject("loginError", "Incorrect password");
+        mav.addObject("loginErrorField", "password");
+        return mav;
+    }
+
+    // Redirect to the index page after successful login
+    return new ModelAndView("redirect:/index");
+    
 }
 
-
-
-
-
+}
