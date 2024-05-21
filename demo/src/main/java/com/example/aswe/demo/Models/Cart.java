b48056dde18f3850ce37faadf42a00d69476f3ff
mariam mahmoud <mervat2105283@miuegypt.edu.eg>
2024-05-21 18:31:37 +0200

    Cart Model

--- demo/src/main/java/com/example/aswe/demo/Models/Cart.java
+++ demo/src/main/java/com/example/aswe/demo/Models/Cart.java
@@ -1,5 +1,51 @@
 package com.example.aswe.demo.Models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Cart {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cartQuantity;
+    private Double TotalPrice;
+
+    @OneToMany
+    private Product product;
+
+    public Cart(){
+    }
+
+    public Cart(int cartQuantity,Double TotalPrice){
+        this.cartQuantity=cartQuantity;
+        this.TotalPrice=TotalPrice;
+    }
+
+    public int getCartQuantity() {
+        return this.cartQuantity;
+    }
+
+    public void setCartQuantity(int cartQuantity) {
+        this.cartQuantity = cartQuantity;
+    }
+
+    public Double getTotalPrice() {
+        return this.TotalPrice;
+    }
+
+    public void setTotalPrice(Double TotalPrice) {
+        this.TotalPrice = TotalPrice;
+    }
+
+    @Override
+    public String toString(){
+        return "{" +
+            " cartQuantity='" + getCartQuantity() + "'" +
+            ", TotalPrice='" + getTotalPrice() + "'" +
+            "}";
+    }
 }
